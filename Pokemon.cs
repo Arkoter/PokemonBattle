2757b591b0d27b9a32a6d69a565d653d0f1dad70
robin <robin.martin67@ynov.com>
2025-10-30 17:01:02 +0100

    Initial commit

---
+++ Pokemon.cs
@@ -0,0 +1,25 @@
+﻿public class Pokemon
+{
+    public string Name { get; }
+    public string Type { get; }
+    public int LifePoints { get; private set; }
+    
+    public Pokemon(string name, string type, int lifePoints)
+    {
+        Name = name;
+        Type = type;
+        LifePoints = lifePoints;
+    }
+    
+    public void Attack(Pokemon target, int Damage)
+    {
+        Console.WriteLine($"{Name} attacks {target.Name} and deals {Damage} damage!");
+        target.ReceiveDamage(Damage);
+    }
+
+    public void ReceiveDamage(int Damage)
+    {
+        LifePoints = Math.Max(0, LifePoints - Damage);
+        Console.WriteLine($"{Name} has {LifePoints} HP left!");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var pikachu = new Pokemon("Pikachu", "Electric", 100);
+        var dracaufeu = new Pokemon("Dracaufeu", "Fire", 100);
+        
+        pikachu.Attack(dracaufeu, 30);
+    }
+}
